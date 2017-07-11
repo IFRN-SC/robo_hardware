b47b27db77fc4273beaa5c4a28340feb270a8e75
lennedy <lennedy@gmail.com>
2017-07-11 18:12:59 -0300

    Simplificação do progrma de acionamento dos motores, além da colocação de alguns comentários

--- examples/LigarMotor/LigarMotor.ino
+++ examples/LigarMotor/LigarMotor.ino
@@ -12,19 +12,25 @@
 
 
 void setup(){
+	//Configura o robo.
 	robo.configurar(false);
 }
 
 void loop(){
+	//Liga os dois motores com 50% da valocidade maxima
 	robo.acionarMotores(50, 50);
 	delay(1000);
-	robo.acionarMotores(0, 50);
-	delay(1000);
+
+	//Desliga os dois motores
 	robo.acionarMotores(0, 0);
 	delay(1000);
-	robo.acionarMotores(-50, 0);
-	delay(1000);
+
+	//Liga os dois motores com 50% da velocidade maxima so que no sentido oposto
 	robo.acionarMotores(-50, -50);
+	delay(1000);
+
+	//Desliga os dois motores
+	robo.acionarMotores(0, 0);
 	delay(1000);
 }
 
