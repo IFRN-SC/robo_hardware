1b745082c995116da533fca390ba01aba5756a0b
lennedy <lennedy@gmail.com>
2017-07-11 20:00:43 -0300

    Remoção de erros  e simplificação do .ino

--- examples/SeguirLinha/SeguirLinha.ino
+++ examples/SeguirLinha/SeguirLinha.ino
@@ -18,7 +18,7 @@
 //
 //*********PINOS PARA SENSORES REFLETANCIA*********//
 
-#define CINZA 50
+#define DIVISOR_BRANCO_PRETO 50
 
 float valor_sensor_dir;
 float valor_sensor_esq;
@@ -28,19 +28,23 @@ void setup(){
 }
 
 void loop(){
-	valor_sensor_dir = robo.lerSensorLinhaEsq();
-	valor_sensor_esq = robo.lerSensorLinhaDir();
 
-	if(valor_sensor_dir > CINZA && valor_sensor_esq > CINZA){
-		robo.acionarMotores(50,50);
+	valor_sensor_esq = robo.lerSensorLinhaEsq(); //Le o valor do sensor esquerdo e coloca dentro da variavel valor_sensor_esq
+	valor_sensor_dir = robo.lerSensorLinhaDir(); //Le o valor do sensor direito e coloca dentro da variavel valor_sensor_dir
+
+	//Identifica se os dois sensores viram branco
+	if(valor_sensor_dir > DIVISOR_BRANCO_PRETO && valor_sensor_esq > DIVISOR_BRANCO_PRETO){
+		robo.acionarMotores(50,50);	//Aciona os dois motores com a mesma velocidade
 	}
-	else if (valor_sensor_dir < CINZA && valor_sensor_esq > CINZA){
-		robo.acionarMotores(0,50);
+	//Identifica se o sensor da esquerda viu banco e o da direita viu preto
+	else if (valor_sensor_dir < DIVISOR_BRANCO_PRETO && valor_sensor_esq > DIVISOR_BRANCO_PRETO){
+		robo.acionarMotores(50,0);	//Aciona o motor esquerdo e mantem o motor direito desligado
 	}
-	else if (valor_sensor_dir > CINZA && valor_sensor_esq < CINZA){
-		robo.acionarMotores(0,50);
+	//Identifica se o sensor da direita viu banco e o da esquerda viu preto
+	else if ( valor_sensor_dir > DIVISOR_BRANCO_PRETO && valor_sensor_esq < DIVISOR_BRANCO_PRETO){
+		robo.acionarMotores(0,50);	//Aciona o motor direito e mantem o motor esquerdo desligado
 	}
-	else{
+	else{ //Identifica se os dois sensores viram preto
 		robo.acionarMotores(0,0);
 	}
 
