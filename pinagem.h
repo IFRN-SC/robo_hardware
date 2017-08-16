24d8865d65f5f12a161fcdd37cd526f31540c797
robotica <you@example.com>
2017-08-16 19:13:26 -0300

    mudança nos pinos da biblioteca para remover incoerência entre os pinos dos sonares e dos sensores de cor. Mudança nos exemplos para tornar mais fácil a compreenção dos pinos que os sensores devem ser ligados.

--- pinagem.h
+++ pinagem.h
@@ -4,27 +4,27 @@
 /*
 Arduino UNO pinout
 
-13-									SONAR_TRIGGER_LATERAL
-12-									SONAR_ECHO_LATERAL
-11-	PWM (Timer 2)
+13-									SENSOR_COR_ESQ_OUT
+12-									SENSOR_COR_ESQ_S3
+11-	PWM (Timer 2)		SENSOR_COR_ESQ_S2
 10-	PWM	(Timer 1)		SERVO_GARRA_1
 09-	PWM	(Timer 1)		SERVO_GARRA_2
-08-									
+08-									SENSOR_COR_DIR_OUT
 07-									SENTIDO_RODA_DIREITA
 06-	PWM (TIMER 0)		PWM_RODA_DIREITA
 05- PWM (TIMER 0)		PWM_RODA_ESQUERDA
 04-									SENTIDO_RODA_ESQUERDA
-03-	PWM	(Timer 2)		SONAR_ECHO_FRONTAL
-02-									SONAR_TRIGGER_FRONTAL
+03-	PWM	(Timer 2)		SENSOR_COR_DIR_S3
+02-									SENSOR_COR_DIR_S2
 01-	Tx
 00-	Rx
 
 A0- 								SENSOR_LINHA_ESQUERDO
 A1-									SENSOR_LINHA_DIREITO
-A2-
-A3-
-A4-	SDA
-A5-	SCL
+A2-									SONAR_TRIGGER_LATERAL
+A3-									SONAR_ECHO_LATERAL
+A4-	SDA							SONAR_TRIGGER_FRONTAL
+A5-	SCL							SONAR_ECHO_FRONTAL
 
 
 */
@@ -44,8 +44,6 @@ class pinagem{
   #define SERVO_GARRA_1 				10	//Define o pino para "controlar" o servo motor 1
   #define SERVO_GARRA_2 				9		//Define o pino para "controlar" o servo motor 2
 
-	//A presente biblioteca pode utilizar um botao para comunicao
-  #define BOTAO 								45
 
 	//A presente biblioteca utiliza dois sensores de refletancia ou sensores de linha
 	#define SENSOR_LINHA_ESQUERDO	A0	//Define o pino do sensor de refletancia esquerdo 
@@ -59,8 +57,8 @@ class pinagem{
 	#define SENSOR_COR_ESQ_OUT		A3	//Pino analogico A3
 
 	#define SENSOR_COR_DIR_S2			2
-	#define SENSOR_COR_DIR_S3			1
-	#define SENSOR_COR_DIR_OUT		0
+	#define SENSOR_COR_DIR_S3			3
+	#define SENSOR_COR_DIR_OUT		8
 
 	//A biblioteca possui dois sensores de distancia (Sonares)
 	#define SONAR_TRIGGER_FRONTAL	2
