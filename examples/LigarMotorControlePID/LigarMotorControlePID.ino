a7988268ab467e0587f7f7a7fc83f200ad2c1155
lennedy <lennedy@gmail.com>
2017-08-21 18:00:08 -0300

    Adicionado exemplo para testes do controle PID

---
+++ examples/LigarMotorControlePID/LigarMotorControlePID.ino
@@ -0,0 +1,36 @@
+#include <robo_hardware2.h> 
+#include <Servo.h>
+
+//*********PINOS PARA LIGAR MOTORES*********//
+//    Motor1:
+//pino1 PWM_RODA_DIREITA 	6
+//pino2 SENTIDO_RODA_DIREITA 	7
+//    Motor2:
+//pino1 PWM_RODA_ESQUERDA 	5
+//pino2 SENTIDO_RODA_ESQUERDA	4
+//*********PINOS PARA LIGAR MOTORES*********//
+
+
+void setup(){
+	//Configura o robo.
+	robo.configurar(false);
+}
+
+void loop(){
+	//Liga os dois motores com 50% da valocidade maxima
+	robo.acionarMotoresVel(50, 50);
+	delay(1000);
+
+	//Desliga os dois motores
+	robo.acionarMotoresVel(0, 0);
+	delay(1000);
+
+	//Liga os dois motores com 50% da velocidade maxima so que no sentido oposto
+	robo.acionarMotoresVel(-50, -50);
+	delay(1000);
+
+	//Desliga os dois motores
+	robo.acionarMotoresVel(0, 0);
+	delay(1000);
+}
+
