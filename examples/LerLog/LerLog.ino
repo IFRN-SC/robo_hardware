8b96d756757b95dd1827ae03303c2a4f6a176342
lennedy <lennedy@gmail.com>
2017-08-25 21:26:54 -0300

    adição de comentários. Acrescentada a chamada da função lerLog() essa chamada de função serve para ler os dados de log presentes na eeprom e atualizar o objeto com essas informações.

--- examples/LerLog/LerLog.ino
+++ examples/LerLog/LerLog.ino
@@ -4,7 +4,8 @@
 Log debug;
 
 void setup(){
-	debug.printDados();
+	debug.lerLog(); //Faz a leitura dos dados presentes na eeprom e atualiza as variáveis do próprio objeto
+	debug.printDados(); //Envia pela porta serial os dados armazenados no objeto
 }
 
 void loop(){
