f88462b64fbd8c4ad100352a31e09cac50dd0429
lennedy <lennedy@gmail.com>
2017-08-26 22:50:33 -0300

    Criação da classe Motor para conter tudo relacionado ao acionamento dos motores DC

---
+++ Motor.cpp
@@ -0,0 +1,75 @@
+#include "Motor.h"
+
+Motor::Motor(byte pino_pwm, byte pino_direcao):
+encoder(0,0),
+PINO_PWM(pino_pwm), 
+PINO_DIRECAO(pino_direcao)
+{
+	controlador_habilitado=false;
+}
+
+Motor::Motor(byte pino_pwm, byte pino_direcao, Encoder e):
+encoder(e),
+PINO_PWM(pino_pwm), 
+PINO_DIRECAO(pino_direcao)
+{
+	controlador_habilitado=true;
+}
+
+void Motor::configurar(){
+	encoder.configurar();
+	pinMode(PINO_PWM, OUTPUT); 
+  pinMode(PINO_DIRECAO, OUTPUT);
+}
+
+void Motor::tensao(float valor_por_cento,int pino){
+  float k = 255/100.0;
+
+  if(valor_por_cento < -100) 
+    valor_por_cento = -100;	//se o valor passado em valor_por_cento for menor que -100 obriga-se o mmotor a ficar em -100
+  if(valor_por_cento >  100) 
+    valor_por_cento =  100;	//se o valor passado em valor_por_cento for maior que 100 obriga-se o mmotor a ficar em 100
+
+  float valor_ate_255 = valor_por_cento * k;
+  
+  analogWrite(pino, abs(valor_ate_255)); 
+}
+
+void Motor::acionarMotores(float motor1){
+ 
+  if(motor1 < 0){
+    digitalWrite(PINO_DIRECAO, false);//Define aqui o sentido de rotacao
+    tensao(motor1,PINO_PWM);  				//Chama a funcao que mudara a intensidade do sinal aplicado
+  }else{
+    digitalWrite(PINO_DIRECAO, true); //Define aqui o sentido de rotacao
+    motor1 = 100 - motor1;			
+    tensao(motor1,PINO_PWM);					//Chama a funcao que mudara a intensidade do sinal aplicado
+  }    
+}
+
+void Motor::acionarMotoresVel(float velMotor){
+	float tensao=0;
+
+//	#if defined(__AVR_ATmega2560__)
+		encoder.calculaVelocidade();
+		contrVelo.executa(encoder.getVelocidade(), velMotor);
+
+		tensao = contrVelo.getOutput();
+//	#endif
+
+	acionarMotores(tensao);
+}
+
+void Motor::acionarMotoresPos(float anguloRef){
+	float tensao=0;
+
+//	#if defined(__AVR_ATmega2560__)
+		contrPos.executa(encoder.getAnguloAbsoluto(), anguloRef);
+
+		tensao = contrPos.getOutput();
+//	#endif
+	//acionar motor
+	acionarMotores(tensao);
+
+}
+
---
+++ Motor.h
@@ -0,0 +1,30 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include "encoder.h"
+#include "contr_motor.h"
+#include "pinagem.h"
+
+class Motor{
+	public:
+	
+	Motor(byte pino_pwm, byte pino_direcao);
+	Motor(byte pino_pwm, byte pino_direcao, Encoder e);
+
+	void configurar();
+	
+	void acionarMotores(float percetualMotor);
+  void acionarMotoresVel(float velMotor);
+	void acionarMotoresPos(float angMotor);
+	
+	private:
+
+	void tensao(float valor_por_cento, int pino);
+
+	Encoder encoder;
+	contr_motor contrVelo, contrPos;
+	bool controlador_habilitado;
+	const byte PINO_PWM, PINO_DIRECAO;
+};
+
+#endif
