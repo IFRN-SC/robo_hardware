993a24cca33ed68731e79e4d248912141b2172e0
lennedy <lennedy@gmail.com>
2017-08-27 23:12:39 -0300

    Adição da classe VarCompartilhada para impedir que uma interrupção ocorra enquanto uma variável compartilhada estiver sendo lida ou escrita.

---
+++ VariavelCompartilhada.cpp
@@ -0,0 +1,20 @@
+#include "VariavelCompartilhada.h"
+/*
+void VariavelCompartilhada::set(T v){
+	//obriga que tudo que estiver dentro do block seja executado de forma atomica
+	//ou seja, desabilita a interrupcao enquanto estiver dentro desse bloco
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){ //chamada de um macro
+		variavel = v;
+	}
+}
+
+T VariavelCompartilhada::get(){
+	T v;
+	//obriga que tudo que estiver dentro do block seja executado de forma atomica
+	//ou seja, desabilita a interrupcao enquanto estiver dentro desse bloco
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){ //chamada de um macro
+		v = variavel;
+	}
+	return v;
+}
+*/
