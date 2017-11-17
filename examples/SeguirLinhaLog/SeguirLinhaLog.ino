bfc108b32182ab29d2e896fdd0fe9ccdcf47effd
robotica <you@example.com>
2017-11-17 17:32:08 -0300

    Modificado o exemplo seguirLinhaLog para se adaptar as mudanças realizadas na classe Log.

--- examples/SeguirLinhaLog/SeguirLinhaLog.ino
+++ examples/SeguirLinhaLog/SeguirLinhaLog.ino
@@ -1,6 +1,7 @@
 #include <robo_hardware2.h> 
 #include <Servo.h>
 #include <Log.h>
+#include <SD.h>
 
 //*************PINOS PARA LIGAR MOTORES*************//
 //   Motor direito:
@@ -21,6 +22,9 @@
 
 #define DIVISOR_BRANCO_PRETO 50
 
+#define LED_VERMELHO 53
+#define LED_AZUL 49
+
 float valor_sensor_dir;
 float valor_sensor_esq;
 
@@ -31,46 +35,68 @@ Log debug;
 DadosLog dados;
 
 void setup(){
+   pinMode(LED_VERMELHO,OUTPUT);
+   pinMode(LED_AZUL,OUTPUT);
+   
+   digitalWrite(LED_AZUL,HIGH);
+   if(debug.configurar()){
+     digitalWrite(LED_VERMELHO, LOW);
+   }
+   else{
+     while(true){
+       digitalWrite(LED_VERMELHO, HIGH);   // turn the LED on (HIGH is the voltage level)
+       delay(1000);               // wait for a second
+       digitalWrite(LED_VERMELHO, LOW);    // turn the LED off by making the voltage LOW
+       delay(1000);               // wait for a second
+     }
+   }
 	robo.configurar(false);
+
 }
 
 void loop(){
-
+        digitalWrite(LED_AZUL,LOW);
 	valor_sensor_esq = robo.lerSensorLinhaEsq(); //Le o valor do sensor esquerdo e coloca dentro da variavel valor_sensor_esq
 	valor_sensor_dir = robo.lerSensorLinhaDir(); //Le o valor do sensor direito e coloca dentro da variavel valor_sensor_dir
 
-	for(int i=0; i<MAX_TAMANHO_LOG; i++){
-		//Identifica se os dois sensores viram branco
-		if(valor_sensor_esq > DIVISOR_BRANCO_PRETO && valor_sensor_dir > DIVISOR_BRANCO_PRETO){
-			//Aciona os dois motores com a mesma velocidade
-			valor_motor_esq=50;
-			valor_motor_dir=50;
-		}
-		//Identifica se o sensor da esquerda viu banco e o da direita viu preto
-		else if (valor_sensor_esq > DIVISOR_BRANCO_PRETO && valor_sensor_dir < DIVISOR_BRANCO_PRETO){
-			//Aciona o motor esquerdo para frente e aciona o motor direito para tras
-			valor_motor_esq=50;
-			valor_motor_dir=-30;				
-		}
-		//Identifica se o sensor da direita viu banco e o da esquerda viu preto
-		else if (valor_sensor_esq < DIVISOR_BRANCO_PRETO && valor_sensor_dir > DIVISOR_BRANCO_PRETO){
-			//Aciona o motor direito para frente e aciona o motor esquerdo para tras
-			valor_motor_esq=-30;
-			valor_motor_dir=50;
-		}
-		else{ //Identifica se os dois sensores viram preto
-			valor_motor_esq=-30;
-			valor_motor_dir=50;				
-		}
-
-		robo.acionarMotores(valor_motor_esq, valor_motor_dir);
-		dados.tempo = millis();
-		dados.motorEsq = valor_motor_esq;
-		dados.motorDir = valor_motor_dir;
-		dados.reflet.esq = valor_sensor_esq;
-		dados.reflet.dir = valor_sensor_dir;
-
-		debug.adicionarDados(dados);
+	//Identifica se os dois sensores viram branco
+	if(valor_sensor_esq > DIVISOR_BRANCO_PRETO && valor_sensor_dir > DIVISOR_BRANCO_PRETO){
+		//Aciona os dois motores com a mesma velocidade
+		valor_motor_esq=-30;
+		valor_motor_dir=-30;
 	}
+	//Identifica se o sensor da esquerda viu banco e o da direita viu preto
+	else if (valor_sensor_esq > DIVISOR_BRANCO_PRETO && valor_sensor_dir < DIVISOR_BRANCO_PRETO){
+		//Aciona o motor esquerdo para frente e aciona o motor direito para tras
+		valor_motor_esq=50;
+		valor_motor_dir=-30;				
+	}
+	//Identifica se o sensor da direita viu banco e o da esquerda viu preto
+	else if (valor_sensor_esq < DIVISOR_BRANCO_PRETO && valor_sensor_dir > DIVISOR_BRANCO_PRETO){
+		//Aciona o motor direito para frente e aciona o motor esquerdo para tras
+		valor_motor_esq=-30;
+		valor_motor_dir=50;
+	}
+	else{ //Identifica se os dois sensores viram preto
+		valor_motor_esq=-30;
+		valor_motor_dir=50;				
+	}
+
+	robo.acionarMotores(valor_motor_esq, valor_motor_dir);
+
+
+
+	dados.salvarTempo();
+	dados.motorEsq = valor_motor_esq;
+	dados.motorDir = valor_motor_dir;
+	dados.reflet.esq = valor_sensor_esq;
+	dados.reflet.dir = valor_sensor_dir;
+
+	debug.adicionarDados(dados);
+
+        digitalWrite(LED_AZUL,HIGH);
+
 	debug.salvarLog();
+
+
 }
