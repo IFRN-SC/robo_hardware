26fa7af6807b92d349439b3062e4bf09bd71a605
PauloVLB <vitorlimaborges@gmail.com>
2018-04-12 11:18:15 -0300

    Adicionados os comentários da classe de cor para melhor entendimento.

--- cor.cpp
+++ cor.cpp
@@ -3,151 +3,149 @@
 
 SensorCor::SensorCor(int pino_sensor_cor_esquerdo, int pino_sensor_cor_direito):PINO_SENSOR_COR_ESQUERDO(pino_sensor_cor_esquerdo), PINO_SENSOR_COR_DIREITO(pino_sensor_cor_direito){ 
 
-  tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
-  pinMode(pino_sensor_cor_esquerdo, OUTPUT);  
-  pinMode(pino_sensor_cor_direito, OUTPUT);    
+  tcsCorHardware = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X); // Instancia o objeto tcsCorHardware da classe Adafruit
+  pinMode(pino_sensor_cor_esquerdo, OUTPUT);  // 
+  pinMode(pino_sensor_cor_direito, OUTPUT);   // Define os pinos do sensor de cor como OUTPUT
   
 }
-void SensorCor::ligar_sensor1 (){
+void SensorCor::ligarSensorEsquerdo (){   // Função que liga o sensor1 e desliga o sensor2
   
   digitalWrite(PINO_SENSOR_COR_ESQUERDO, LOW);
   digitalWrite(PINO_SENSOR_COR_DIREITO, HIGH);
-  delay(4);
-  tcs.begin(); 
+  delay(4);    // Delay para pode inicializar o sensor (se diminuido ou retirado, o sensor não conseguira inicializar).
+  tcsCorHardware.begin(); // Método "begin" (começo) da classe do Adafruit. Inicializa o sensor. 
   
 }
 
-void SensorCor::ligar_sensor2 (){
+void SensorCor::ligarSensorDireito (){  // Função que liga o sensor2 e desliga o sensor1
   digitalWrite(PINO_SENSOR_COR_DIREITO, LOW);
   digitalWrite(PINO_SENSOR_COR_ESQUERDO, HIGH);
   delay(4);
-  tcs.begin();  
+  tcsCorHardware.begin();  
 }
 
-void SensorCor::ler_sensor (){
+void SensorCor::lerSensor (){ // Função para ler os valores de R, G e B.
   
   delay(60);  
-  tcs.getRawData(&red, &green, &blue, &clear);
+  tcsCorHardware.getRawData(&red, &green, &blue, &clear); // Método da classe Adafruit que lê os dados "brutos" do sensor.
   
 }
 
 
-void SensorCor::atualizarRGB_esquerdo(){
-  ligar_sensor1();
-  ler_sensor();
+void SensorCor::atualizarRGB_esquerdo(){ // Liga o sensor em questão e o lê
+  ligarSensorEsquerdo();
+  lerSensor();
   
-  rgb_esquerdo.vermelho = red; 
-  rgb_esquerdo.verde = green;
-  rgb_esquerdo.azul = blue;
+  rgbEsquerdo.vermelho = red; //
+  rgbEsquerdo.verde = green;  //
+  rgbEsquerdo.azul = blue;    // Coloca os valores lidos em sua respectivas variáveis da struct RGB
 
 }
 
-void SensorCor::atualizarRGB_direito(){
-  ligar_sensor2();
-  ler_sensor();
+void SensorCor::atualizarRGB_direito(){  // Liga o sensor em questão e o lê
+  ligarSensorDireito();
+  lerSensor();
   
-  rgb_direito.vermelho = red; 
-  rgb_direito.verde = green;
-  rgb_direito.azul = blue;
+  rgbDireito.vermelho = red;  // 
+  rgbDireito.verde = green;   //
+  rgbDireito.azul = blue;     // Coloca os valores lidos em sua respectivas variáveis da struct RGB
 
 }
 
-RGB SensorCor::getRGB_esquerdo(){
+RGB SensorCor::getRGB_esquerdo(){ // Atualiza o valor do sensor (o lê) e retorna os 3 valores: R, G e B.
   atualizarRGB_esquerdo();
- return rgb_esquerdo;
+ return rgbEsquerdo;
 }
 
-RGB SensorCor::getRGB_direito(){
+RGB SensorCor::getRGB_direito(){ // Atualiza o valor do sensor (o lê) e retorna os 3 valores: R, G e B.
   atualizarRGB_direito();
-  return rgb_direito;
+  return rgbDireito;
 }
 
-int SensorCor::calculeMaximo(int vermelho, int verde, int azul){
-  int Maximo = 0; 
+int SensorCor::calculeMaximo(int vermelho, int verde, int azul){ // Calcula o maior valor entre vermelho, verde e azul
+  int maximo = 0; // Armazena o menor valor entre os 3.
   if(verde >= vermelho && verde >= azul){
-    Maximo = verde;
+    maximo = verde;
   }else if(vermelho >= verde && vermelho >= azul){
-    Maximo = vermelho;
+    maximo = vermelho;
   }else if(azul >= verde && azul >= vermelho){
-    Maximo = azul;
+    maximo = azul;
   }
-  return Maximo;
+  return maximo;
 }
 
-int SensorCor::calculeMinimo(int vermelho, int verde, int azul){
-  int Minimo = 0;
+int SensorCor::calculeMinimo(int vermelho, int verde, int azul){ // Calcula o menor valor entre vermelho, verde e azul
+  int minimo = 0; // Armazena o menor valor entre os 3.
   if(verde <= vermelho && verde <= azul){
-    Minimo = verde;
+    minimo = verde;
   }else if(vermelho <= verde && vermelho <= azul){
-    Minimo = vermelho;
+    minimo = vermelho;
   }else if(azul <= verde && azul <= vermelho){
-    Minimo = azul;
+    minimo = azul;
   }
-  return Minimo;
-}
-
-HSV SensorCor::getHSV_esquerdo(){
-	atualizarRGB_esquerdo();
-	return getHSV(rgb_esquerdo);
-	
-}
-
-HSV SensorCor::getHSV_direito(){
-	atualizarRGB_direito();
-	return getHSV(rgb_direito);
-	
-	
+  return minimo;
 } 
 
-HSV SensorCor::getHSV(RGB rgb){
+HSV SensorCor::getHSV(RGB rgb){ // Converte os valores de RGB para HSV. 
 	
-  HSV hsv;
+  HSV valorHsv;
 		
-  double Maximo = 0;
-  double Minimo = 0;
+  double maximo = 0; // 
+  double minimo = 0; // variávels que armazenaram o maior e o menor valores entre o vermelho, verde e azul
   
-  int vermelho =  rgb.vermelho;
-  int azul = rgb.azul;
-  int verde = rgb.verde;
+  int vermelhoRGB =  rgb.vermelho; //
+  int azulRGB = rgb.azul;          //
+  int verdeRGB = rgb.verde;        // Variável que recebem os valores de suas respectivas cores em RGB
   
-  Minimo = calculeMinimo(vermelho,verde,azul);
-  Maximo = calculeMaximo(vermelho,verde,azul);
+  minimo= calculeMinimo(vermelhoRGB,verdeRGB,azulRGB); //
+  maximo = calculeMaximo(vermelhoRGB,verdeRGB,azulRGB);// Recebem o maior e o menor valores entre o vermelho, verde e azul   
   
   
-  if(Maximo == vermelho && verde >= azul){
-    hsv.h =  60 * (verde - azul)/ (Maximo - Minimo)  ; 
-  }else if(Maximo == vermelho && verde < azul){
-    hsv.h =  60 * (verde - azul)/(Maximo - Minimo) + 360;
-  }else if(Maximo == verde){
-    hsv.h =  60 * (azul - vermelho)/(Maximo - Minimo) + 120;
-  }else if(Maximo == azul){
-    hsv.h =  60 * (vermelho - verde)/(Maximo - Minimo) + 240;
-  }
+  if(maximo == vermelhoRGB && verdeRGB >= azulRGB){
+    valorHsv.h =  60 * (verdeRGB - azulRGB)/ (maximo - minimo); 
+  }else if(maximo == vermelhoRGB && verdeRGB < azulRGB){
+    valorHsv.h =  60 * (verdeRGB - azulRGB)/(maximo - minimo) + 360;
+  }else if(maximo == verdeRGB){
+    valorHsv.h =  60 * (azulRGB - vermelhoRGB)/(maximo - minimo) + 120;
+  }else if(maximo == azulRGB){
+    valorHsv.h =  60 * (vermelhoRGB - verdeRGB)/(maximo - minimo) + 240;
+  } // Lógica que define o valor do H com base em cálculos
   
-  hsv.s =  (Maximo - Minimo) / Maximo;
-  hsv.v = Maximo;
-  return hsv;
+  valorHsv.s =  (maximo - minimo) / maximo; // 
+  valorHsv.v = maximo;                      // Definem o S e V da struct HSV
+  return valorHsv;     // Retorna os valores H, S e V.
 }
 
-int SensorCor::getVerde_esquerdo(){
-  return rgb_esquerdo.verde;
+HSV SensorCor::getHSV_esquerdo(){         //
+  atualizarRGB_esquerdo();                //
+  return getHSV(rgbEsquerdo);             // Atualiza os valores do RGB, os converte para HSV e os retornam.  
+  
 }
 
-int SensorCor::getAzul_esquerdo(){
-  return rgb_esquerdo.azul;
-}
-int SensorCor::getVermelho_esquerdo(){	
-  return rgb_esquerdo.vermelho;
+HSV SensorCor::getHSV_direito(){         //
+  atualizarRGB_direito();                //
+  return getHSV(rgbDireito);             // Atualiza os valores do RGB, os converte para HSV e os retornam.
 }
 
-int SensorCor::getVerde_direito(){
-  return rgb_direito.verde;
+int SensorCor::getVerdeEsquerdo(){ // Retorna o valor do Verde em RGB               
+  return rgbEsquerdo.verde;                      
+}                                                 
+                                                
+int SensorCor::getAzulEsquerdo(){ // Retorna o valor do Azul em RGB              
+  return rgbEsquerdo.azul;
+}
+int SensorCor::getVermelhoEsquerdo(){ // Retorna o valor do Vermelho em RGB	
+  return rgbEsquerdo.vermelho;
 }
 
-int SensorCor::getAzul_direito(){
-  return rgb_direito.azul;
+int SensorCor::getVerdeDireito(){ // Retorna o valor do Verde em RGB
+  return rgbDireito.verde;
 }
 
-int SensorCor::getVermelho_direito(){
-  return rgb_direito.vermelho;
+int SensorCor::getAzulDireito(){ // Retorna o valor do Azul em RGB
+  return rgbDireito.azul;
+}
+
+int SensorCor::getVermelhoDireito(){ // Retorna o valor do Vermelho em RGB
+  return rgbDireito.vermelho;
 }
--- cor.h
+++ cor.h
@@ -4,14 +4,14 @@
 #include <Arduino.h>
 #include "Adafruit_TCS34725.h"
 
-struct RGB{
+struct RGB{   // Struct com valores de verde, vermelho e azul.
   int verde;
   int vermelho;
   int azul;
 
 };
 
-struct HSV{
+struct HSV{ // Struct com valores do HSV.
   float h;
   double s;
   float v;
@@ -24,45 +24,43 @@ public:
   //s0 e s1 não inicializados, pois vão ficar com valores lógicos permanentes (s0 -> HIGH, s1 -> LOW)	
   SensorCor(int pino_sensor_cor_esquerdo, int pino_sensor_cor_direito);
   //metodo para receber o valor atual do sensor RGB	
-  void atualizarRGB_esquerdo();
-  void atualizarRGB_direito();
+  void atualizarRGB_esquerdo(); // Liga o sensor em questão e o lê
+  void atualizarRGB_direito();  // Liga o sensor em questão e o lê
 	
-  int getVerde_esquerdo();
-  int getVermelho_esquerdo();
-  int getAzul_esquerdo();
+  int getVerdeEsquerdo(); // Retorna o valor do Verde em RGB
+  int getVermelhoEsquerdo(); // Retorna o valor do Vermelho em RGB
+  int getAzulEsquerdo();      // Retorna o valor do Azul em RGB
   
-  int getVerde_direito();
-  int getVermelho_direito();
-  int getAzul_direito();
+  int getVerdeDireito(); // Retorna o valor do Verde em RGB
+  int getVermelhoDireito(); // Retorna o valor do Vermelho em RGB
+  int getAzulDireito();     // Retorna o valor do Azul em RGB
   
-  RGB getRGB_esquerdo();
-  HSV getHSV_esquerdo();
+  RGB getRGB_esquerdo(); // Atualiza o valor do sensor (o lê) e retorna os 3 valores: R, G e B.
+  HSV getHSV_esquerdo(); // Atualiza os valores do RGB, os converte para HSV e os retornam.
   
-  RGB getRGB_direito();
-  HSV getHSV_direito();
+  RGB getRGB_direito(); // Atualiza o valor do sensor (o lê) e retorna os 3 valores: R, G e B.
+  HSV getHSV_direito(); // Atualiza os valores do RGB, os converte para HSV e os retornam.
   
   
     
 private:
-  int calculeMaximo(int vermelho, int verde, int azul);     
-  int calculeMinimo(int vermelho, int verde, int azul);
-
-	//float saturar(float val);
+  int calculeMaximo(int vermelho, int verde, int azul);  // Calcula o maior valor entre vermelho, verde e azul   
+  int calculeMinimo(int vermelho, int verde, int azul);   // Calcula o menor valor entre vermelho, verde e azul
   
-  RGB rgb_direito;
-  RGB rgb_esquerdo;
+  RGB rgbDireito;  // Struct para os valores do sensor direito
+  RGB rgbEsquerdo; // Struct para os valores do sensor esquerdo 
  
-  Adafruit_TCS34725 tcs;
+  Adafruit_TCS34725 tcsCorHardware; // Objeto tcsCorHardware da classe Adafruit
 
-  uint16_t clear, red, green, blue; 
+  uint16_t clear, red, green, blue; // Variáveis de leitura para a classe da Adafruit.
   
-  void ligar_sensor1();
-  void ligar_sensor2();
-  void ler_sensor();
-  HSV getHSV(RGB rgb);
+  void ligarSensorEsquerdo(); // Função que liga o sensor1 e desliga o sensor2
+  void ligarSensorDireito(); // Função que liga o sensor2 e desliga o sensor1
+  void lerSensor(); // Função para ler os valores de R, G e B.
+  HSV getHSV(RGB rgb); // Converte os valores de RGB para HSV.
   
-  const int PINO_SENSOR_COR_ESQUERDO;
-  const int PINO_SENSOR_COR_DIREITO;
+  const int PINO_SENSOR_COR_ESQUERDO; // Constante do pino que vai para o arduino
+  const int PINO_SENSOR_COR_DIREITO;  // Constante do pino que vai para o arduino
   
   
   
