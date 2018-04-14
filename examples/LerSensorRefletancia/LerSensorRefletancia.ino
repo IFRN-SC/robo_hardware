7a38042f1dd12c40310d60fba3e443e2a0a94f13
lennedy <lennedy@gmail.com>
2018-04-13 21:06:10 -0300

    Mudanção dos sensores para ficarem na mesma linha.

--- examples/LerSensorRefletancia/LerSensorRefletancia.ino
+++ examples/LerSensorRefletancia/LerSensorRefletancia.ino
@@ -20,11 +20,10 @@ void loop(){
 	valor_sensor_esq = robo.lerSensorLinhaEsq(); //le um valor do sensor. O sensor retornar um valor de 0 100
 	valor_sensor_dir = robo.lerSensorLinhaDir(); //le um valor do sensor. O sensor retornar um valor de 0 100
 
-	Serial.print("Valor do sensor direito é: ");
+	Serial.print("Sensores maisEsq; esq; dir; maisDir: ");
+	Serial.print(valor_sensor_esq); //Imprime o valor do sensor pela porta serial
+	Serial.print("; ");
 	Serial.println(valor_sensor_dir); //Imprime o valor do sensor pela porta serial
-
-	Serial.print("Valor do sensor esquerdo é: ");
-	Serial.println(valor_sensor_esq); //Imprime o valor do sensor pela porta serial
 
 	delay(1000);
 
