95d7912be987b62a8f9f6a41b495d88ebda50ffe
lennedy <lennedy@gmail.com>
2018-04-14 17:21:11 -0300

    modificação do exemplo de refletãncia

--- examples/LerSensorRefletancia/LerSensorRefletancia.ino
+++ examples/LerSensorRefletancia/LerSensorRefletancia.ino
@@ -20,7 +20,7 @@ void loop(){
 	valor_sensor_esq = robo.lerSensorLinhaEsq(); //le um valor do sensor. O sensor retornar um valor de 0 100
 	valor_sensor_dir = robo.lerSensorLinhaDir(); //le um valor do sensor. O sensor retornar um valor de 0 100
 
-	Serial.print("Sensores maisEsq; esq; dir; maisDir: ");
+	Serial.print("Sensores refletancia: +esq; esq; dir; +dir ");
 	Serial.print(valor_sensor_esq); //Imprime o valor do sensor pela porta serial
 	Serial.print("; ");
 	Serial.println(valor_sensor_dir); //Imprime o valor do sensor pela porta serial
