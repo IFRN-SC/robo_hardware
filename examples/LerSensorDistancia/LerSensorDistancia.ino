a432e9a597f1bc04ab979ea37e6276c49654c4c5
lennedy <lennedy@gmail.com>
2018-04-14 22:55:31 -0300

    Modificação dos comentários para ter os pinos dos arduinos uno e mega

--- examples/LerSensorDistancia/LerSensorDistancia.ino
+++ examples/LerSensorDistancia/LerSensorDistancia.ino
@@ -2,11 +2,14 @@
 #include <Servo.h>
 
 //********PINOS PARA SENSORES DE DISTANCIA***********//
-//  SONAR_TRIGGER_FRONTAL	A4
-//  SONAR_ECHO_FRONTAL		A5
+//  SONAR_TRIGGER_FRONTAL	uno-A4; mega-16
+//  SONAR_ECHO_FRONTAL		uno-A5; mega-17
 
-//  SONAR_TRIGGER_LATERAL	A2
-//  SONAR_ECHO_LATERAL		A3
+//  SONAR_TRIGGER_ESQ	uno-0; mega-22
+//  SONAR_ECHO_ESQ		uno-1; mega-24
+
+//  SONAR_TRIGGER_DIR	uno-0; mega-23
+//  SONAR_ECHO_DIR		uno-1; mega-25
 //*********PINOS PARA SENSORES DE DISTANCIA*********//
 
 float valorSensorFrontal;
