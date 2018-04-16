0b53202f75547c55f0e3cb2eade0fc465232d9b6
lennedy <lennedy@gmail.com>
2018-04-15 22:31:53 -0300

    Criação do arquivo de exemplo para ligar os leds

---
+++ examples/LigarLeds/LigarLeds.ino
@@ -0,0 +1,43 @@
+#include <robo_hardware2.h> 
+#include <Servo.h>
+
+//********PINOS PARA LEDS***********//
+//
+//LED1			uno-;	mega-53
+//LED2			uno-;	mega-51
+//LED3			uno-;	mega-49
+//
+//*********PINOS PARA LEDS*********//
+
+#define DELAY_TIME 500
+
+void setup(){
+	//Configura o robo.
+	robo.configurar(false);
+}
+
+void loop(){
+	for(int led=1; led<=3; led++){
+		robo.ligarLed(led);
+		delay(DELAY_TIME);
+	}
+	for(int led=1; led<=3; led++){
+		robo.desligarLed(led);
+		delay(DELAY_TIME);
+	}
+	for(int led=3; led<=1; led--){
+		robo.ligarLed(led);
+		delay(DELAY_TIME);
+	}
+	for(int led=3; led<=1; led--){
+		robo.desligarLed(led);
+		delay(DELAY_TIME);
+	}
+	
+	robo.ligarTodosLeds();
+	delay(DELAY_TIME);
+
+	robo.desligarTodosLeds();
+	delay(DELAY_TIME);
+}
+
