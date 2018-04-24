4486ba2024eb9743ec17db2200fa9b3fadd3319e
robotica@sc11506 <robotica@sc11506>
2018-04-24 11:09:00 -0300

    Declaração, e criação de funções para o sonar lateral direito

--- robo_hardware2.cpp
+++ robo_hardware2.cpp
@@ -4,25 +4,23 @@
 Servo robo_hardware::servoGarra1;
 Servo robo_hardware::servoGarra2;
 
-
-robo_hardware::robo_hardware():	corDireita	(SENSOR_COR_DIR_S2,SENSOR_COR_DIR_S3,SENSOR_COR_DIR_OUT),
-																corEsquerda	(SENSOR_COR_ESQ_S2,SENSOR_COR_ESQ_S3,SENSOR_COR_ESQ_OUT),
-																sonarFrontal(SONAR_TRIGGER_FRONTAL, SONAR_ECHO_FRONTAL),
-																sonarLateral(SONAR_TRIGGER_LATERAL, SONAR_ECHO_LATERAL)
+robo_hardware::robo_hardware():	
+  corDireita(SENSOR_COR_DIR_S2,SENSOR_COR_DIR_S3,SENSOR_COR_DIR_OUT),
+  corEsquerda(SENSOR_COR_ESQ_S2,SENSOR_COR_ESQ_S3,SENSOR_COR_ESQ_OUT),
+  sonarFrontal(SONAR_TRIGGER_FRONTAL, SONAR_ECHO_FRONTAL),
+  sonarLateralEsquerdo(SONAR_TRIGGER_LADO_ESQUERDO, SONAR_ECHO_LADO_ESQUERDO), 
+  sonarLateralDireito(SONAR_TRIGGER_LADO_DIREITO,SONAR_ECHO_LADO_DIREITO)
 {
-
 }
 
 void robo_hardware::tensao(float valor_por_cento,int pino){
   float k = 255/100.0;
-
   if(valor_por_cento < -100) 
-    valor_por_cento = -100;	//se o valor passado em valor_por_cento for menor que -100 obriga-se o mmotor a ficar em -100
+	valor_por_cento = -100;	//se o valor passado em valor_por_cento for menor que -100 obriga-se o mmotor a ficar em -100
   if(valor_por_cento >  100) 
-    valor_por_cento =  100;	//se o valor passado em valor_por_cento for maior que 100 obriga-se o mmotor a ficar em 100
+	valor_por_cento =  100;	//se o valor passado em valor_por_cento for maior que 100 obriga-se o mmotor a ficar em 100
 
   float valor_ate_255 = valor_por_cento * k;
-  
   analogWrite(pino, abs(valor_ate_255)); 
 }
 
@@ -30,62 +28,65 @@ void robo_hardware::tensao(float valor_por_cento,int pino){
 void robo_hardware::configurar(bool habilitar_garra){
   Serial.begin(9600);
 
-	//Com essas funcoes os sonares sao calibrados 
-	//Quanto maior o valor de CALIBRACAO_SONAR menor a inclinacao da curva de calibracao 
-	sonarFrontal.setDivisor(CALIBRACAO_SONAR, Ultrasonic::CM);  
-	sonarLateral.setDivisor(CALIBRACAO_SONAR, Ultrasonic::CM);
+  //Com essas funcoes os sonares sao calibrados 
+  //Quanto maior o valor de CALIBRACAO_SONAR menor a inclinacao da curva de calibracao 
+  sonarFrontal.setDivisor(CALIBRACAO_SONAR, Ultrasonic::CM);  
+  sonarLateralDireito.setDivisor(CALIBRACAO_SONAR, Ultrasonic::CM);
+  sonarLateralEsquerdo.setDivisor(CALIBRACAO_SONAR, Ultrasonic::CM);
   
-	if(habilitar_garra){
-  	servoGarra1.attach(SERVO_GARRA_1);
-  	servoGarra2.attach(SERVO_GARRA_2);
-	}
+  if(habilitar_garra){
+	servoGarra1.attach(SERVO_GARRA_1);
+	servoGarra2.attach(SERVO_GARRA_2);
+  }
 
+  //pinMode(BOTAO, INPUT_PULLUP);
   pinMode(PWM_RODA_ESQUERDA, OUTPUT); 
   pinMode(PWM_RODA_DIREITA, OUTPUT); 
   pinMode(SENTIDO_RODA_ESQUERDA, OUTPUT); 
   pinMode(SENTIDO_RODA_DIREITA, OUTPUT);
-
-//  pinMode(BOTAO, INPUT_PULLUP);
 }
 
 //boolean robo_hardware::lerSensorFimDeCurso(){
 //  return digitalRead(BOTAO); 
 //}
 
-int robo_hardware::lerSensorDeLinha(int sensor){
-  return analogRead(sensor);
+float robo_hardware::lerSensorDeLinha(int sensor){
+  return (100 - 100.0 * (analogRead(sensor))/1023.0);;
 }
 
 void robo_hardware::acionarMotores(float motor1, float motor2){
- 
   if(motor1 < 0){
-    digitalWrite(SENTIDO_RODA_ESQUERDA, false);
-    tensao(motor1,PWM_RODA_ESQUERDA);  
+	digitalWrite(SENTIDO_RODA_ESQUERDA, false);
+	tensao(motor1,PWM_RODA_ESQUERDA);  
   }else{
-    digitalWrite(SENTIDO_RODA_ESQUERDA, true);
-    motor1 = 100 - motor1;
-    tensao(motor1,PWM_RODA_ESQUERDA);  
+ 	digitalWrite(SENTIDO_RODA_ESQUERDA, true);
+	motor1 = 100 - motor1;
+	tensao(motor1,PWM_RODA_ESQUERDA);  
   }
   
   if(motor2 < 0){
-    digitalWrite(SENTIDO_RODA_DIREITA, false);
+	digitalWrite(SENTIDO_RODA_DIREITA, false);
     tensao(motor2,PWM_RODA_DIREITA);  
   }else{
-    digitalWrite(SENTIDO_RODA_DIREITA, true);
-    motor2 = 100 - motor2;
-    tensao(motor2,PWM_RODA_DIREITA);  
-  }
-    
+	digitalWrite(SENTIDO_RODA_DIREITA, true);
+	motor2 = 100 - motor2;
+	tensao(motor2,PWM_RODA_DIREITA);  
+  } 
 }
 
 float robo_hardware::lerSensorSonarFrontal(){
-	long microsec = sonarFrontal.timing();
-	return sonarFrontal.convert(microsec, Ultrasonic::CM);  //retorna a distância do sensor ao obstáculo em cm.
+  long microsec = sonarFrontal.timing();
+  return sonarFrontal.convert(microsec, Ultrasonic::CM);  //retorna a distância do sensor ao obstáculo em cm.
 }
 
-float robo_hardware::lerSensorSonarLateral(){
-	long microsec = sonarLateral.timing();
-	return sonarLateral.convert(microsec, Ultrasonic::CM);  //retorna a distância do sensor ao obstáculo em cm.
+float robo_hardware::lerSensorSonarLateralEsquerdo(){
+  long microsec = sonarLateralEsquerdo.timing();
+  return sonarLateralEsquerdo.convert(microsec, Ultrasonic::CM);  //retorna a distância do sensor ao obstáculo em cm.
+}
+
+float robo_hardware::lerSensorSonarLateralDireito(){
+  long microsec = sonarLateralDireito.timing();
+  return sonarLateralDireito.convert(microsec, Ultrasonic::CM);  //retorna a distância do sensor ao obstáculo em cm.
 }
 
 void robo_hardware::acionarServoGarra1(float angulo){
@@ -113,9 +114,9 @@ RGB robo_hardware::getRGBDireito(){
 }
 
 void  robo_hardware::salvarCalibracao(calibracao_dados calibraca_val){
-	EEPROM.put(ENDERECO_EEPROM, calibraca_val);
+  EEPROM.put(ENDERECO_EEPROM, calibraca_val);
 }
 
 void robo_hardware::lerCalibracao(calibracao_dados &cal){
-	EEPROM.get(ENDERECO_EEPROM, cal);
+  EEPROM.get(ENDERECO_EEPROM, cal);
 }
--- robo_hardware2.h
+++ robo_hardware2.h
@@ -1,29 +1,24 @@
-
 //nesse arquivo as funcoes devem ser somente declaradas
 #ifndef ROBO_HARDWARE_H
 #define ROBO_HARDWARE_H
 
-
 #include <Arduino.h>
 #include <Servo.h>
-
 #include "cor.h"
 #include "pinagem.h"
 #include "Ultrasonic.h"
-
 #include "EEPROM2.h"
 
 struct calibracao_dados{
-	HSV branco;
-	HSV preto;
-	HSV verde;
-	HSV cinza;
+   HSV branco;
+   HSV preto;
+   HSV verde;
+   HSV cinza;
 
-	int refletancia_dir;
-	int refletancia_mais_dir;
-	int refletancia_esq;
-	int refletancia_mais_esq;
-	
+   int refletancia_dir;
+   int refletancia_mais_dir;
+   int refletancia_esq;
+   int refletancia_mais_esq;
 
 };
 
@@ -31,57 +26,57 @@ class robo_hardware:private pinagem{
 private:
 
   #define AJUSTE_MOTOR 0.65
-	#define MAX_10_BITS 1023.0				//Maior valor que um numero de 10 bits pode obter
-	
-	#define CALIBRACAO_SONAR  40.4	//Valor para calibrar os sonares. Quanto maior esse valor menor a inclinação da reta de calibracao
+  #define MAX_10_BITS 1023.0		//Maior valor que um numero de 10 bits pode obter	
+  #define CALIBRACAO_SONAR  40.4	//Valor para calibrar os sonares. Quanto maior esse valor menor a inclinação da reta de calibracao
+  #define ENDERECO_EEPROM 0
 
-	#define ENDERECO_EEPROM 0
 public: 
 
-
   robo_hardware();
-  void configurar(bool habilitar_garra=true);
-//  boolean lerSensorFimDeCurso();
+  void configurar(bool habilitar_garra=true); //boolean lerSensorFimDeCurso();
 
-	//As funcoes retornam o valor lido do sensor refletancia
-  int lerSensorDeLinha(int sensor); //recebe um pino analogico (A0, A1, A2 e etc) e retorna um valor de 0 a 1023 
-	inline float lerSensorLinhaEsq(){return (100 - 100.0 * (lerSensorDeLinha(SENSOR_LINHA_ESQUERDO))/1023.0);} //retorna um valor de 0 a 100 
-	inline float lerSensorLinhaDir(){return (100 - 100.0 * (lerSensorDeLinha(SENSOR_LINHA_DIREITO) )/1023.0);} //retorna um valor de 0 a 100
+  //As funcoes retornam o valor lido do sensor refletancia
+  float lerSensorDeLinha(int sensor);                         						     //retorna um valor de 0 a 1023 
+  inline float lerSensorLinhaEsq(){return lerSensorDeLinha(SENSOR_LINHA_ESQUERDO);} //retorna um valor de 0 a 100   
+  inline float lerSensorLinhaDir(){return lerSensorDeLinha(SENSOR_LINHA_DIREITO);} //retorna um valor de 0 a 100
+  inline float lerSensorLinhaMaisDir(){return lerSensorDeLinha(SENSOR_LINHA_MAIS_DIREITO);} //retorna um valor de 0 a 100
+  inline float lerSensorLinhaMaisEsq(){return lerSensorDeLinha(SENSOR_LINHA_MAIS_ESQUERDO);} //retorna um valor de 0 a 100
 
-	//A funcao para acionar os motores de locomocao do robo
-	//A funcao recebe um percentual de tensao do motor esquerdo e direito
-	//A funcao so recebe valores que variem de 100 ate -100
+  //A funcao para acionar os motores de locomocao do robo
+  //A funcao recebe um percentual de tensao do motor esquerdo e direito
+  //A funcao so recebe valores que variem de 100 ate -100
   void acionarMotores(float percetualMotorEsquerdo, float percetualMotorDireito);
 
-	//funcao para acionar os servomotores
+  //funcao para acionar os servomotores
   void acionarServoGarra1(float angulo);
   void acionarServoGarra2(float angulo);
 
-	float lerSensorSonarFrontal();
-	float lerSensorSonarLateral();
+  float lerSensorSonarFrontal();
+  float lerSensorSonarLateralEsquerdo();
+  float lerSensorSonarLateralDireito();
   
-  
-	//funcoes para trabalhar com os sensores de Cor
+  //funcoes para trabalhar com os sensores de Cor
   HSV getHSVEsquerdo(); //realiza a leitura do sensor de cor esquerdo e retorna uma estrutura HSV. Ver cor.h
   RGB getRGBEsquerdo(); //realiza a leitura do sensor de cor esquerdo e retorna uma estrutura RGB. Ver cor.h
   HSV getHSVDireito(); 	//realiza a leitura do sensor de cor direito e retorna uma estrutura HSV. Ver cor.h
   RGB getRGBDireito(); 	//realiza a leitura do sensor de cor direito e retorna uma estrutura RGB. Ver cor.h
 
-	void salvarCalibracao(calibracao_dados cal);  
-	void lerCalibracao(calibracao_dados &cal);
+  void salvarCalibracao(calibracao_dados cal);  
+  void lerCalibracao(calibracao_dados &cal);
 
 private:
-
 
   static Servo servoGarra1;
   static Servo servoGarra2;
   void tensao(float valor_por_cento,int pino);
+
   //SensorCor cor_direita(s2,s3, out);  
   SensorCor corDireita;
   SensorCor corEsquerda;
 
-	Ultrasonic sonarFrontal;
-	Ultrasonic sonarLateral;  
+  Ultrasonic sonarFrontal;
+  Ultrasonic sonarLateralEsquerdo;  
+  Ultrasonic sonarLateralDireito;  
 
 };
 
