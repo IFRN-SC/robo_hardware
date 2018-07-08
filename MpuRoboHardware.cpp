733c81a325e669f11159a800680991a54171c8a0
lennedy <lennedy@gmail.com>
2018-07-07 21:04:38 -0300

    Remoção de código de debug
    Foram comentados códigos de debug para serem substituídos futuramente por
    um objeto que realize a mesma função.

--- MpuRoboHardware.cpp
+++ MpuRoboHardware.cpp
@@ -6,7 +6,6 @@ volatile bool MpuRoboHardware::mpuInterrupt=false;
 
 void MpuRoboHardware::configurar(){
 	dmpReady = false;
-//	teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
 	mpuInterrupt = false;
 
   // join I2C bus (I2Cdev library doesn't do this automatically)
@@ -17,25 +16,25 @@ void MpuRoboHardware::configurar(){
       Fastwire::setup(400, true);
   #endif
 
-  Serial.begin(115200);
+  //Serial.begin(115200);
 
-  Serial.println(F("Initializing I2C devices..."));
+  //Serial.println(F("Initializing I2C devices..."));
 
   mpu.initialize();
   pinMode(INTERRUPT_PIN, INPUT);
 
   // verify connection
-  Serial.println(F("Testing device connections..."));
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+  //Serial.println(F("Testing device connections..."));
+  //Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
   // wait for ready
-  Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-  while (Serial.available() && Serial.read()); // empty buffer
-  while (!Serial.available());                 // wait for data
-  while (Serial.available() && Serial.read()); // empty buffer again
+  //Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+  //while (Serial.available() && Serial.read()); // empty buffer
+  //while (!Serial.available());                 // wait for data
+  //while (Serial.available() && Serial.read()); // empty buffer again
 
   // load and configure the DMP
-  Serial.println(F("Initializing DMP..."));
+  //Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
 
   // supply your own gyro offsets here, scaled for min sensitivity
@@ -48,16 +47,16 @@ void MpuRoboHardware::configurar(){
   // make sure it worked (returns 0 if so)
   if (devStatus == 0) {
       // turn on the DMP, now that it's ready
-      Serial.println(F("Enabling DMP..."));
+//      Serial.println(F("Enabling DMP..."));
       mpu.setDMPEnabled(true);
 
       // enable Arduino interrupt detection
-      Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+//      Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
       attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
       mpuIntStatus = mpu.getIntStatus();
 
       // set our DMP Ready flag so the main loop() function knows it's okay to use it
-      Serial.println(F("DMP ready! Waiting for first interrupt..."));
+//      Serial.println(F("DMP ready! Waiting for first interrupt..."));
       dmpReady = true;
 
       // get expected DMP packet size for later comparison
@@ -67,9 +66,9 @@ void MpuRoboHardware::configurar(){
       // 1 = initial memory load failed
       // 2 = DMP configuration updates failed
       // (if it's going to break, usually the code will be 1)
-      Serial.print(F("DMP Initialization failed (code "));
-      Serial.print(devStatus);
-      Serial.println(F(")"));
+//      Serial.print(F("DMP Initialization failed (code "));
+//      Serial.print(devStatus);
+//      Serial.println(F(")"));
   }
 
 }
