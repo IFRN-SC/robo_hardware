1b00e763c1681fcae76aab7ce393ae0439158081
lennedy <lennedy@gmail.com>
2018-09-27 17:28:46 -0300

    Exemplo modificado
    Modificado exemplo para utilizar o robo_hadware

--- examples/LerEncoder/LerEncoder.ino
+++ examples/LerEncoder/LerEncoder.ino
@@ -1,23 +1,22 @@
-#include <Wire.h>
-#include <EncoderI2C.h>
+#include <robo_hardware2.h>
+#include <Servo.h>
 
-EncoderI2C encoder(ENDERECO_ENCODER_DIR);
-
-void setup(){
-    Serial.begin(9600);
-    encoder.setPulsosPorRevolucao(224.4);//muda de acordo com o tipo de motor
+void setup() {
+  robo.configurar(false);  
+  Serial.begin(9600);
 }
 
-void loop(){
-  long l=0;
-  float a=0;
 
-  Serial.print("AngRelativo e AngAbsoluto: ");
-  l=encoder.getAnguloRelativo();
-  a=encoder.getAngulo();
-  Serial.print(l);
+void loop() {
+  float angEsq=0, angDir=0;
+  
+  angEsq = robo.getAngEncoderEsq();
+  angDir = robo.getAngEncoderDir();
+
+  Serial.print("Angulo esquerdo e direito: ");
+  Serial.print(angEsq);
   Serial.print("; ");
-  Serial.println(a);
+  Serial.println(angDir);
 
-  delay(500);
+  delay(1000);
 }
