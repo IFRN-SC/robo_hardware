bb516779a4ba7277220798721500bf99c66e6edc
lennedy <lennedy@gmail.com>
2018-09-28 16:52:22 -0300

    Remoção de bug
    Modificação do link pelo arquivo original

--- EncoderI2C.cpp
+++ EncoderI2C.cpp
@@ -1,1 +1,41 @@
-/home/lennedy/software/LeitorEncoder/EncoderI2C.cpp
+#include "EncoderI2C.h"
+
+EncoderI2C::EncoderI2C(int address){
+	ultimosPulsos = 0;
+	pulsosPorRevolucao=224.4;
+	Wire.begin();
+	ATtinyAddress= address;
+}
+
+long EncoderI2C::getData(){
+
+	uint8_t byteArray[DATA_SIZE];
+
+	for(int i=0; i<DATA_SIZE; i++ ){
+		Wire.beginTransmission(ATtinyAddress);
+		Wire.write(i);
+		Wire.endTransmission();
+		Wire.requestFrom(ATtinyAddress, 1);
+
+		while(Wire.available()){ 
+				byteArray[i] = Wire.read(); 
+		}
+	}
+
+	return conv.arrayToLong(byteArray);
+}
+
+float EncoderI2C::getAngulo(){
+	return pulsosParaAngulo(getPulsos());
+}
+
+long EncoderI2C::getPulsos(){
+	long pulsos = getData();
+	ultimosPulsos = pulsos;
+	return pulsos;
+}
+
+float EncoderI2C::getAnguloRelativo(){
+	long pulsos=ultimosPulsos;
+	return (getPulsos() - pulsos);
+}
--- EncoderI2C.h
+++ EncoderI2C.h
@@ -1,1 +1,75 @@
-/home/lennedy/software/LeitorEncoder/EncoderI2C.h
+#ifndef ENCODER_I2C_H
+#define ENCODER_I2C_H
+
+#include <Arduino.h>
+//#ifdef AVR_ATtiny85
+//	#include <TinyWireS.h>
+//#else
+#include <Wire.h>
+//#include <Conversao.h>
+//#endif
+
+#define DATA_SIZE 4
+#define ENDERECO_ENCODER_DIR 0X26
+#define ENDERECO_ENCODER_ESQ 0X27
+
+class Conversao{
+  public:
+  long arrayToLong(const uint8_t* c){
+    unsigned long temp=0;
+ 		for(int i=DATA_SIZE-1; i>=0; i--){
+		  temp+=(unsigned long)c[i];
+			if(i>0)
+			  temp<<=8;  
+		}
+//  temp = (long)c[0];
+    return (long)temp;
+  }
+
+  void longToArray(const long t, uint8_t* c){
+    c[0] = t>>24;
+    c[1] = t>>16;
+    c[2] = t>>8;
+    c[3] = t & 0xff;
+  }
+  
+  void intToChar(const int t, uint8_t* c){
+    c[0] = t>>8;
+    c[1] = t & 0xff;
+  }
+
+	void initTime(){
+		time = micros();
+	}
+
+	unsigned long timePassed(){
+		return ( micros() - time );
+	}
+	private:
+		unsigned long time;
+
+};
+
+
+class EncoderI2C{
+	public:
+		EncoderI2C(int address);
+
+		long getPulsos();
+		float getAngulo();
+		float getAnguloRelativo();
+
+		inline void setPulsosPorRevolucao(float ppr){pulsosPorRevolucao=ppr;}
+
+	private:
+		int ATtinyAddress;
+		float pulsosPorRevolucao;
+		Conversao conv;
+		long ultimosPulsos;
+
+		long getData();
+
+		float pulsosParaAngulo(long pulsos){return 360*(pulsos/pulsosPorRevolucao);}
+};
+
+#endif
