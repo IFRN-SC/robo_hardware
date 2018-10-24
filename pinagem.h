f75786a36b813b0d3bc4a5db0cc803fa9060fcfa
leticiaBonita <leticiassantos15@outlook.com>
2018-10-24 10:14:44 -0300

    Troquei a ordem da pinagem direito com o mais direito

--- pinagem.h
+++ pinagem.h
@@ -33,11 +33,10 @@ A5-	SCL							SONAR_ECHO_FRONTAL
 class pinagem{
 
   //***PARA ARDUINO UNO E PRO MINI DEVE-SE UTILIZAR PARA MOTORES DC O PWM SOMENTE NAS PORTAS 5 E 6 ***/// 
-  #define PWM_RODA_ESQUERDA 		  6		//Define o pino que ligara o motor esquerdo para mudar a velocidade de rotacao/amarelo 
-  #define SENTIDO_RODA_ESQUERDA 	7		//Define o pino que ligara o motor esquerdo para mudar o sentido de rotacao/verde
-  
+  #define PWM_RODA_ESQUERDA 		   6		//Define o pino que ligara o motor esquerdo para mudar a velocidade de rotacao/amarelo 
+  #define SENTIDO_RODA_ESQUERDA 	 7
 	//***PARA ARDUINO UNO E PRO MINI DEVE-SE UTILIZAR PARA MOTORES DC O PWM SOMENTE NAS PORTAS 5 E 6 ***///
-  #define PWM_RODA_DIREITA 		    5		//Define o pino que ligara o motor direito para mudar a velocidade de rotacao/amarelo 
+  #define PWM_RODA_DIREITA 		    5	//Define o pino que ligara o motor direito para mudar a velocidade de rotacao/amarelo 
   #define SENTIDO_RODA_DIREITA 		4		//Define o pino que ligara o motor direito para mudar o sentido de rotacao/azul
 
   //A presente biblioteca pode controlar dois servomotores  
@@ -48,8 +47,8 @@ class pinagem{
   //*********PINOS PARA SENSORES DE REFLETANCIA*********//
   #define SENSOR_LINHA_MAIS_ESQUERDO	 A1	//Define o pino do sensor de refletancia esquerdo 
   #define SENSOR_LINHA_ESQUERDO	       A2	//Define o pino do sensor de refletancia esquerdo 
-  #define SENSOR_LINHA_DIREITO	       A0	//Define o pino do sensor de refletancia direito
-  #define SENSOR_LINHA_MAIS_DIREITO		 A3	//Define o pino do sensor de refletancia direito
+  #define SENSOR_LINHA_DIREITO	       A3	//Define o pino do sensor de refletancia direito
+  #define SENSOR_LINHA_MAIS_DIREITO		 A0		//Define o pino do sensor de refletancia direito
 
 	//A biblioteca possui dois sensores de cor: um esquerdo e outro direito.
 	//cada sensor de cor precisa de 3 pinos para configuracao e leitura da cor
