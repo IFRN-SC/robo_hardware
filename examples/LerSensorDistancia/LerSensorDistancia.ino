7c380b373b45f8fea9ec24759e51bff798169fe2
LeticiaSSN <leticiassantos15@outlook.com>
2019-10-03 15:22:24 -0300

    Adequado o branch para ficar com a pinagem ideal..

--- examples/LerSensorDistancia/LerSensorDistancia.ino
+++ examples/LerSensorDistancia/LerSensorDistancia.ino
@@ -5,11 +5,11 @@
 //  SONAR_TRIGGER_FRONTAL	uno-A4; mega-16
 //  SONAR_ECHO_FRONTAL		uno-A5; mega-17
 
-//  SONAR_TRIGGER_ESQ	uno-1; mega-22
-//  SONAR_ECHO_ESQ		uno-0; mega-24
+//  SONAR_TRIGGER_ESQ	uno-0; mega-22
+//  SONAR_ECHO_ESQ		uno-1; mega-24
 
-//  SONAR_TRIGGER_DIR	uno-1; mega-23
-//  SONAR_ECHO_DIR		uno-0; mega-25
+//  SONAR_TRIGGER_DIR	uno-0; mega-23
+//  SONAR_ECHO_DIR		uno-1; mega-25
 //*********PINOS PARA SENSORES DE DISTANCIA*********//
 
 float valorSensorFrontal;
